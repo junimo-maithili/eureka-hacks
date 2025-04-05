f10e127fbd7291e9145f88dee563523322424de0
junimo-maithili <126294399+junimo-maithili@users.noreply.github.com>
2025-04-05 15:24:54 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,11 +1,8 @@
 import streamlit as st
+import mainfile
+from mainfile import bars
 
-    
-if 'counter' not in st.session_state:
-    st.session_state.level = 0
-    st.session_state.hunger = 0
-    st.session_state.thirst = 0
-    st.session_state.happiness = 0
+
 
 with st.container(height=220):
     level_bar = st.progress(st.session_state.level, text="Level: %i" % st.session_state.level)
